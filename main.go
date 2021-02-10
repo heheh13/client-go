6d49417f4623fdd92489bcb8993b4b9d01fa2306
Mehedi Hasan <mehedi.hasan@appscode.com>
2021-02-10 12:43:27 +0600

    swithing to main.backup

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/heheh13/client-go/cmd"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/kr/pretty"
+
+	appsv1 "k8s.io/api/apps/v1"
+)
+
+var (
+	deplJson = "./file.json"
+)
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
+	file, err := os.Open(deplJson)
+	if err != nil {
+		panic(err)
+	}
+	dec := json.NewDecoder(file)
+	//var dep extensionsv1beta1.Deployment
+	var dep appsv1.Deployment
+	dec.Decode(&dep)
+	fmt.Println(dep)
+	//dec.Decode(&dep)
+	//pretty.Println(dep)
+	pretty.Println(&dep)
+
 }
